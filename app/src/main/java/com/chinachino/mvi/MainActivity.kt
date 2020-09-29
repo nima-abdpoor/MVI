a276b5d8669a627cb8364ad1fa65d8f5b1786a60
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-29 12:02:02 +0330

    MainFragment.kt and show it

--- app/src/main/java/com/chinachino/mvi/MainActivity.kt
+++ app/src/main/java/com/chinachino/mvi/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMainFragment()
+    }
+    private fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container,MainFragment(),"MainFragment")
+            .commit()
     }
 }
---
+++ app/src/main/java/com/chinachino/mvi/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.chinachino.mvi
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
